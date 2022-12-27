0779e68ba66ff6f152898c5e53fb5f58d3e349d3
Anna Zozulia <ennyqwerty0@gmail.com>
2022-12-27 18:15:49 +0200

    updated files lesson3

--- src/main/java/com/hillel/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lessons/lesson3/Main.java
@@ -3,5 +3,11 @@ package com.hillel.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
+
+        String str = "Hello!";
+        String str1 = new String("hello");
+
+        System.out.println(str);
+        System.out.println(str1);
     }
 }
