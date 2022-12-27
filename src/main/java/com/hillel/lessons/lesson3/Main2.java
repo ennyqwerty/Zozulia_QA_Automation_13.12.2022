45af3e31a98a52623df390c9cc32e99d4dc42dec
ennyqwerty <121312126+ennyqwerty@users.noreply.github.com>
2022-12-27 18:25:26 +0200

    Update Main2.java
    from github

--- src/main/java/com/hillel/lessons/lesson3/Main2.java
+++ src/main/java/com/hillel/lessons/lesson3/Main2.java
@@ -3,5 +3,6 @@ package com.hillel.lessons.lesson3;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
+        System.out.println("Hello, java!");
     }
 }
