ec3b80c88d1ca8e997e4596f474de12d511dd25d
Anna Zozulia <ennyqwerty0@gmail.com>
2022-12-28 01:42:55 +0200

    added HomeWork4

---
+++ src/main/java/com/hillel/lessons/lesson4/HomeWork4.java
@@ -0,0 +1,17 @@
+package com.hillel.lessons.lesson4;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+        int c = 8;
+
+        int volume = a*b*c;
+        int length = 4*(a+b+c);
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println();
+        System.out.println("Довжина всіх ребер паралелепіпеда = " + length);
+    }
+}
---
+++ src/main/java/com/hillel/lessons/lesson4/Main.java
@@ -0,0 +1,37 @@
+package com.hillel.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+//      System.out.println(a + b);
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+
+//        System.out.println((double) a /(double) b);
+
+//        System.out.println( a % b);
+
+        a = a + 5;
+
+        System.out.println(a);
+
+        a++;
+        a++;
+        a++;
+
+        System.out.println(a);
+
+        a--;
+
+        System.out.println(a);
+
+        int c = 10;
+
+        System.out.println(10 + a++);
+
+    }
+}
