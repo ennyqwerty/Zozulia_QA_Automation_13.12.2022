a5cf0f66f545c29f160711f9790089b7bd2361e6
Anna Zozulia <ennyqwerty0@gmail.com>
2022-12-27 18:33:18 +0200

    updated files lesson3

--- src/main/java/com/hillel/lessons/lesson3/HomeWork3.java
+++ src/main/java/com/hillel/lessons/lesson3/HomeWork3.java
@@ -1,6 +1,6 @@
 package com.hillel.lessons.lesson3;
 
-public class Main2 {
+public class HomeWork3 {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
         System.out.println("Hello, java!");
