647fc9bad2b163fc995894e132152c8b45d8b439
Anna Zozulia <ennyqwerty0@gmail.com>
2023-01-11 11:35:38 +0200

    fixed homework 7

--- src/main/java/com/hillel/lessons/lesson6/HomeWork7GuessNumber.java
+++ src/main/java/com/hillel/lessons/lesson6/HomeWork7GuessNumber.java
@@ -6,24 +6,25 @@ public class HomeWork7GuessNumber {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    int setComputerNumber = (int) Math.round(Math.random() * 10);
 
+    System.out.println("Число, яке задане комп'ютером = " + setComputerNumber);
 
     int setPlayerNumber = 0;
     for (int i = 1; i <= 3; i++) {
-
-      int setComputerNumber = (int) (Math.random() * 11);
-      System.out.println("Число, яке задане комп'ютером = " + setComputerNumber);
-
       System.out.println("Будь ласка, введіть число від 1 до 10");
+
+
 
       if (scanner.hasNextInt()) {
         setPlayerNumber = scanner.nextInt();
-      } if (setComputerNumber == setPlayerNumber) {
-        System.out.println("Вітаємо ви вгадали число");{
+      }
+      if (setComputerNumber == setPlayerNumber) {
+        System.out.println("Вітаємо, ви вгадали число");{
           break;
         }
       } else {
-        System.out.println("Вибачте, але ви не вгадали число");
+        System.out.println("Вибачте, але ви не вгадали число.");
         scanner.nextLine();
       }
 
