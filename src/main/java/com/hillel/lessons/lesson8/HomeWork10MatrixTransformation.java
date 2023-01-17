193459d5704720b6eb601cfb864f2fde03dad442
Anna Zozulia <ennyqwerty0@gmail.com>
2023-01-17 19:16:58 +0200

    updated HW10

--- src/main/java/com/hillel/lessons/lesson8/HomeWork10MatrixTransformation.java
+++ src/main/java/com/hillel/lessons/lesson8/HomeWork10MatrixTransformation.java
@@ -67,7 +67,7 @@ public class HomeWork10MatrixTransformation {
 
         for (int i = 0; i < array2.length; i++) {
             for (int j = 0; j < array2[i].length; j++) {
-                System.out.print(array2[j][i] + "\t");
+                System.out.print(array1[j][i] + "\t");
             }
             System.out.println();
         }
